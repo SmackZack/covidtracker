65f2519c46c9bcade7ee63a4a5fa29b1b00e58fd
SmackZack <ashishone9781@gmail.com>
2020-11-21 13:10:28 +0530

    changes to datatable

--- js/data.js
+++ js/data.js
@@ -35,4 +35,42 @@ $.ajax(settings).done(function (response) {
 });
 });
 
+$(document).ready(function () {
+  $("#dtMaterialDesignExample").DataTable({
+    
+    info: false,
+    paging: false,
+    fnInitComplete: function () {
+      var myCustomScrollbar = document.querySelector(
+        "#dt-fixed-footer_wrapper .dataTables_scrollBody"
+      );
+      var ps = new PerfectScrollbar(myCustomScrollbar);
+    },
+    scrollY: 450,
+  });
+  $("#dtMaterialDesignExample_wrapper")
+    .find("label")
+    .each(function () {
+      $(this).parent().append($(this).children());
+    });
+  $("#dtMaterialDesignExample_wrapper .dataTables_filter")
+    .find("input")
+    .each(function () {
+      const $this = $(this);
+      $this.attr("placeholder", "Search");
+      $this.removeClass("form-control-sm");
+    });
+  $("#dtMaterialDesignExample_wrapper .dataTables_length").addClass(
+    "d-flex flex-row"
+  );
+  $("#dtMaterialDesignExample_wrapper .dataTables_filter").addClass("md-form");
+  $("#dtMaterialDesignExample_wrapper select").removeClass(
+    "custom-select custom-select-sm form-control form-control-sm"
+  );
+  $("#dtMaterialDesignExample_wrapper select").addClass("mdb-select");
+  $("#dtMaterialDesignExample_wrapper .mdb-select").materialSelect();
+  $("#dtMaterialDesignExample_wrapper .dataTables_filter")
+    .find("label")
+    .remove();
+});
 
